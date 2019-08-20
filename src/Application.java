d761cc9d38e065ac7d98696f96966221cea38990
Jonathas F. Silva <jonathas_ssilva@hotmail.com>
2019-08-20 11:31:01 -0300

    entrega1

---
+++ src/Application.java
@@ -0,0 +1,10 @@
+public class Application {
+
+    public static void main(String[] args) {
+        HelloWorld helloWorld = new HelloWorld("Jonathas");
+        HelloWorld helloWorld1 = new HelloWorld("Jonathas F. Silva");
+
+        helloWorld.imprimir();
+        helloWorld1.imprimir();
+    }
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,30 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class HelloWorld {
+    private String nome;
+
+    public HelloWorld(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void imprimir(){
+        System.out.println(this.getHora()+" - Ola "+this.getNome()+". Voce acabou de fazer seu primeiro Hello World em Java. Parabens.");
+    }
+
+    public String getHora(){
+        DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+        Date date = new Date();
+        String hora = dateFormat.format(date);
+        return hora;
+    }
+}
